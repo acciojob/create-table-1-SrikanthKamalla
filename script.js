6a23d0268726b1cda50a1f5959a28f53b981eb40
Srikanth Kamalla <113651024+SrikanthKamalla@users.noreply.github.com>
2025-03-01 11:36:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function insert_Row() {
     //Write your code here
-  
+	let table=document.querySelector("#sampleTable");
+	let tr=document.createElement("tr");
+	tr.innerHTML=`<td>New Cell1</td><td>New Cell2</td>`
+	table.prepend(tr);
   
 }
